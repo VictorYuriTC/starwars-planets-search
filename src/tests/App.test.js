56951be33a6fdaf79ebce6f4aa6c35c701c6c27e
Victor Yuri <victoryuritc@yahoo.com>
2022-08-18 18:49:05 -0300

    getting elements with mock

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
+import mockData from './helpers/mockData'
 
 describe('Filters component suite tests', () => {
   it('should filter planets by name', () => {
@@ -22,5 +23,25 @@ describe('Filters component suite tests', () => {
       userEvent.selectOptions(comparisonFilter, 'maior que')
       userEvent.type(valueFilter, '1000')
       userEvent.click(buttonFilter)
+  });
+
+  it('should fetch the correct API after component mounting', async () => {
+    render(<App />)
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(mockData),
+    }));
+
+    const planet = await screen.findByText(/alderaan/i);
+    const climate = await screen.findByText(/temperate/i); 
+    const terrain = await screen.findByText(/grasslands, mountains/i);
+    const orbitalPeriod = await screen.findByText(/364/i);
+    const diameter = await screen.findByText(/12500/i);
+    const gravity = await screen.findByText(/1 standard/i);
+    const surfaceWater = await screen.findByText(/40/i);
+    const population = await screen.findByText(/2000000000/i);
+    const films = await screen.findByText(/alderaan/i);
+    const created = await screen.findByText(/alderaan/i); 
+    const edited = await screen.findByText(/alderaan/i); 
+    const url = await screen.findByText(/alderaan/i);
   })
 });
