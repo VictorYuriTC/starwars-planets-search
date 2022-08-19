8777e9cbbcd6298caaef45a2c497d7411c4677ec
Victor Yuri <victoryuritc@yahoo.com>
2022-08-19 09:45:57 -0300

    tests for useNumericNumbers hook

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 import mockData from './helpers/mockData'
+import useNumericFilters from '../hooks/useNumericFilters';
 
 describe('Filters component suite tests', () => {
   it('should filter planets by name', () => {
@@ -12,36 +13,95 @@ describe('Filters component suite tests', () => {
       userEvent.type(nameFilter, 'Naboo')
   });
 
-  it('should filter planets by numeric info', () => {
+  it('should filter planets by numeric info', async () => {
     render(<App />);
+
       const columnFilter = screen.getByTestId('column-filter');
       const comparisonFilter = screen.getByTestId('comparison-filter');
       const valueFilter = screen.getByTestId('value-filter');
       const buttonFilter = screen.getByTestId('button-filter');
     
+      userEvent.selectOptions(columnFilter, 'population')
+      userEvent.selectOptions(comparisonFilter, 'maior que')
+      userEvent.type(valueFilter, '1000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'population')
+      userEvent.selectOptions(comparisonFilter, 'menor que')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'population')
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'rotation_period')
+      userEvent.selectOptions(comparisonFilter, 'maior que')
+      userEvent.type(valueFilter, '1000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'rotation_period')
+      userEvent.selectOptions(comparisonFilter, 'menor que')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'rotation_period')
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'orbital_period')
+      userEvent.selectOptions(comparisonFilter, 'maior que')
+      userEvent.type(valueFilter, '1000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'orbital_period')
+      userEvent.selectOptions(comparisonFilter, 'menor que')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'orbital_period')
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'diameter')
+      userEvent.selectOptions(comparisonFilter, 'maior que')
+      userEvent.type(valueFilter, '1000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'diameter')
+      userEvent.selectOptions(comparisonFilter, 'menor que')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'diameter')
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
       userEvent.selectOptions(columnFilter, 'surface_water')
       userEvent.selectOptions(comparisonFilter, 'maior que')
       userEvent.type(valueFilter, '1000')
       userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'surface_water')
+      userEvent.selectOptions(comparisonFilter, 'menor que')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      userEvent.selectOptions(columnFilter, 'surface_water')
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+      userEvent.type(valueFilter, '10000')
+      userEvent.click(buttonFilter)
+
+      await waitFor(() => expect(screen.findByText(/naboo/i)).toBeInTheDocument())
   });
 
   it('should fetch the correct API after component mounting', async () => {
     render(<App />)
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve(mockData),
-    }));
 
-    const planet = await screen.findByText(/alderaan/i);
-    const climate = await screen.findByText(/temperate/i); 
-    const terrain = await screen.findByText(/grasslands, mountains/i);
-    const orbitalPeriod = await screen.findByText(/364/i);
-    const diameter = await screen.findByText(/12500/i);
-    const gravity = await screen.findByText(/1 standard/i);
-    const surfaceWater = await screen.findByText(/40/i);
-    const population = await screen.findByText(/2000000000/i);
-    const films = await screen.findByText(/alderaan/i);
-    const created = await screen.findByText(/alderaan/i); 
-    const edited = await screen.findByText(/alderaan/i); 
-    const url = await screen.findByText(/alderaan/i);
+    const ALDERAAN_MOVIES = 'https://swapi-trybe.herokuapp.com/api/films/1/https://swapi-trybe.herokuapp.com/api/films/3/https://swapi-trybe.herokuapp.com/api/films/4/https://swapi-trybe.herokuapp.com/api/films/5/https://swapi-trybe.herokuapp.com/api/films/6/'
   })
 });
