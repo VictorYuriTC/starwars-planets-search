ab506430d2d4b0a8f1398784776ded1fe6cdc963
Victor Yuri <victoryuritc@yahoo.com>
2022-08-24 15:06:24 -0300

    FilterPlanetsProvider added to tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
+import FilterPlanetsProvider from '../context/FilterPlanetsProvider';
 
 describe('Filters component suite tests', () => {
   it('should filter planets by name', () => {
-      render(<App />);
+      render(
+        <FilterPlanetsProvider>
+          <App />
+        </FilterPlanetsProvider>
+      );
 
       const nameFilter = screen.getByTestId('name-filter');
       userEvent.type(nameFilter, 'Naboo')
   });
 
   it('should filter planets by numeric info', async () => {
-    render(<App />);
+    render(
+    <FilterPlanetsProvider>
+      <App />
+    </FilterPlanetsProvider>
+    );
 
       const columnFilter = screen.getByTestId('column-filter');
       const comparisonFilter = screen.getByTestId('comparison-filter');
